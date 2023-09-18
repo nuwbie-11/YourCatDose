0a3114bb998be8aeeb48963e7ae97c4fb62efed1
Sadhu <adwityasadhu@gmail.com>
2023-09-18 13:43:31 +0700

    Fixing

--- my-app/src/Components/Header/header.tsx
+++ my-app/src/Components/Header/header.tsx
@@ -14,7 +14,7 @@ export function Header() {
 
   return (
     <div className={`fixed top-[1rem] w-screen text-center ${ isScrolled ? "text-transparent" : "text-zinc-50" } transition ease-in-out duration-200`}>
-      <h1 className="text-xl lg:text-3xl font-bold ">YOUR FURRBALL FACTS!!</h1>
+      <h1 className="text-xl lg:text-3xl font-bold ">FURRBALL FACTS!!</h1>
     </div>
   );
 }
--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -19,10 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`bg-zinc-950 text-zinc-50 ${usedFonts.className}`}>
         <Header></Header>
-        <div className="">
-          {children}
+        
+        {children}
 
-        </div>
       </body>
     </html>
   );
